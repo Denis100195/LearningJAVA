5ce93af91bfda4160c3e63d480da533b66e1462c
Denis100195 <denfil1995@gmail.com>
2021-08-08 21:53:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Arrays.ReplaceElements;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {17,18,5,4,6,1};
+        ReplaceElements replaceElements = new ReplaceElements();
+        replaceElements.replaceElements(arr);
+    }
+}
