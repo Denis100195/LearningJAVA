ae651df091a4ab45167b4812364902638b1225e5
Denis100195 <denfil1995@gmail.com>
2021-08-20 11:56:12 +0300

    perfect square

---
+++ src/main/java/QueueStack/PerfectSquares.java
@@ -0,0 +1,31 @@
+package QueueStack;
+/*Given an integer n, return the least number of perfect square numbers that sum to n.
+
+A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.*/
+
+public class PerfectSquares {
+    public int numSquares(int n) {
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++){
+            int min = Integer.MAX_VALUE;
+
+            for (int j = 1; j*j <= i; j++){
+                int rem = i - j * j;
+                if (dp[rem] < min){
+                    min = dp[rem];
+                }
+            }
+            dp[i] = min + 1;
+        }
+        
+
+
+        return dp[n];
+    }
+}
---
+++ src/test/java/QueueStack/PerfectSquaresTest.java
@@ -0,0 +1,12 @@
+package QueueStack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PerfectSquaresTest {
+
+    @Test
+    public void numSquares() {
+    }
+}
