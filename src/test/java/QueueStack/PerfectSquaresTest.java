124d55f044a9c047b75361b88c44a6df9298165c
Denis100195 <denfil1995@gmail.com>
2021-08-20 13:10:50 +0300

    perfect square

--- src/test/java/QueueStack/PerfectSquaresTest.java
+++ src/test/java/QueueStack/PerfectSquaresTest.java
@@ -1,5 +1,6 @@
 package QueueStack;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,5 +9,8 @@ public class PerfectSquaresTest {
 
     @Test
     public void numSquares() {
+        PerfectSquares perfectSquares = new PerfectSquares();
+        int result = perfectSquares.numSquares(12);
+        Assert.assertEquals(3, result);
     }
 }
