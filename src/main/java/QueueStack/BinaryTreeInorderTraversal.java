70f99b2c24ef812776cdbdeb95a8ef06cc857ebb
Denis100195 <denfil1995@gmail.com>
2021-08-31 15:33:22 +0300

    binary tree inorder traversal

--- src/main/java/QueueStack/BinaryTreeInorderTraversal.java
+++ src/main/java/QueueStack/BinaryTreeInorderTraversal.java
@@ -1,5 +1,45 @@
 package QueueStack;
 
-public class BinaryTreeInorderTraversal {
+import sun.reflect.generics.tree.Tree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        addRoot(root, list);
+        return list;
+    }
+
+    //Recursion
+    public void addRoot(TreeNode root, List<Integer> result){
+        if (root != null){
+            if (root.left != null){
+                addRoot(root.left, result);
+            }
+            result.add(root.val);
+            if (root.right != null){
+                addRoot(root.right, result);
+            }
+        }
+    }
+    //Iterating method using Stack
+    public List<Integer> inorderTraversalStack(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()){
+            while (current != null){
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+        return result;
+    }
 }
---
+++ src/test/java/QueueStack/BinaryTreeInorderTraversalTest.java
@@ -0,0 +1,16 @@
+package QueueStack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinaryTreeInorderTraversalTest {
+
+    @Test
+    public void inorderTraversal() {
+    }
+
+    @Test
+    public void inorderTraversalStack() {
+    }
+}
