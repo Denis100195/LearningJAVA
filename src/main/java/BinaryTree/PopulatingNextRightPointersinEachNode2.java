c02b929750669c16f3623e606826338371bea6e2
Denis100195 <denfil1995@gmail.com>
2021-10-18 17:15:01 +0300

    populating next right point2

--- src/main/java/BinaryTree/PopulatingNextRightPointersinEachNode2.java
+++ src/main/java/BinaryTree/PopulatingNextRightPointersinEachNode2.java
@@ -1,4 +1,30 @@
 package BinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PopulatingNextRightPointersinEachNode2 {
+    public Node connect(Node root){
+        if(root == null)
+            return root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int s = queue.size();
+            for(int i = 0; i < s; i++){
+                Node curr = queue.remove();
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+                if(curr.right != null){
+                    queue.add(curr.right);
+                }
+                if(i == s - 1)
+                    curr.next = null;
+                else
+                    curr.next = queue.peek();
+            }
+        }
+        return root;
+    }
 }
