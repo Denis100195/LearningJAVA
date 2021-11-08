c3ba49eafb418813ff36c15696ba37fec071db99
Denis100195 <denfil1995@gmail.com>
2021-11-08 23:27:30 +0300

    Begin topic Recursion. Reverse String

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 import Arrays.EcpectedNumbers;
+import Recursion.ReverseString;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,1,4,2,1,3};
-        EcpectedNumbers ecpectedNumbers = new EcpectedNumbers();
-        ecpectedNumbers.heightChecker(arr);
+        char[] arr = {'h','e','l','l','o'};
+        ReverseString reverseString = new ReverseString();
+        reverseString.reverseString(arr);
     }
 }
