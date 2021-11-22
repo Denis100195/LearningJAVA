4440e67f5b91d7ce491200a9e90866cb23e9074a
Denis100195 <denfil1995@gmail.com>
2021-11-22 22:42:47 +0300

    K-th Symbol in Grammar

--- src/main/java/Recursion/KSymbolGrammar.java
+++ src/main/java/Recursion/KSymbolGrammar.java
@@ -2,13 +2,13 @@ package Recursion;
 
 public class KSymbolGrammar {
     public int kthGrammar(int n, int k) {
-        if(n==1 || k==1)
+        if(n == 1 || k == 1)
             return 0;
-        int length = (int)Math.pow(2,n-1);
-        int mid = length/2;
+        int length = (int) Math.pow(2,n - 1);
+        int mid = length / 2;
         if(k <= mid)
-            return kthGrammar(n-1,k);
+            return kthGrammar(n - 1, k);
         else
-            return kthGrammar(n-1,k-mid)==0?1:0;
+            return kthGrammar(n - 1,k - mid) == 0 ? 1 : 0;
     }
 }
