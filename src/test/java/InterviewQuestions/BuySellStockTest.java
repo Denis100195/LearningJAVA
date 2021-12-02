67c8317a9f8745cec1aa890fe2cc0e93822df00f
Denis100195 <denfil1995@gmail.com>
2021-12-02 21:50:00 +0300

    Best Time to Buy and Sell Stock II

--- src/test/java/InterviewQuestions/BuySellStockTest.java
+++ src/test/java/InterviewQuestions/BuySellStockTest.java
@@ -1,5 +1,18 @@
+package InterviewQuestions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BuySellStockTest {
 
+    int[] prices = {7,1,5,3,6,4};
+    BuySellStock buySellStock = new BuySellStock();
+
+    @Test
+    public void maxProfit() {
+        int res = buySellStock.maxProfit(prices);
+        Assert.assertEquals(res, 7);
+    }
 }
