d558cff2c3d62356264f07728440cc7366941c97
Denis100195 <denfil1995@gmail.com>
2021-12-07 21:39:18 +0300

    Contains Duplicate

--- src/main/java/InterviewQuestions/ContainsDuplicate.java
+++ src/main/java/InterviewQuestions/ContainsDuplicate.java
@@ -1,12 +1,15 @@
 package InterviewQuestions;
 
+import java.util.Arrays;
+
 /*Given an integer array nums, return true if any value appears at least
 twice in the array, and return false if every element is distinct.*/
 public class ContainsDuplicate {
     public boolean containsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length; i++){
-            for (int j = i+1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) return true;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i + 1]){
+                return true;
             }
         }
         return false;
