977446f0f34ba21fd8a0180841ce5f0637ccaddf
Denis100195 <denfil1995@gmail.com>
2021-12-18 18:17:01 +0300

    Valid Sudoku more grammar

--- src/main/java/InterviewQuestions/ValidSudoku.java
+++ src/main/java/InterviewQuestions/ValidSudoku.java
@@ -9,53 +9,67 @@ package InterviewQuestions;
  * */
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
-
-        for (int i = 0; i < board[0].length; i++){
-            for (int j = 0; j < board[0].length; j++){
-                if (board[i][j] != '.'){
-                    char cur = board[i][j];
-
-                    if(!reccheck(board, i, j)){
+        for(int i=0;i<board[0].length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j] != '.'){
+                    if(reccheck(board,i,j) == false){
                         return false;
                     }
-
-                    
-                    for (int k = j + 1; k < board[0].length - j; k++){
-                        if (board[i][k] == cur) return false;
+                    if(rowcheck(board,i,j) == false){
+                        return false;
                     }
-                    for (int k = i + 1; k < board[0].length - i; k++){
-                        if (board[k][j] == cur) return false;
+                    if(colcheck(board,i,j) == false){
+                        return false;
                     }
                 }
             }
         }
         return true;
     }
-
     public boolean reccheck(char[][] board,int i,int j){
         char temp = board[i][j];
         int temp1 = i;
         int temp2 = j;
-        int r = i + 3;
+        int r = i+3;
         int c = j+3;
-        if(r % 3 != 0){
-            while(r % 3 != 0){
+        if(r%3!=0){
+            while(r%3!=0){
                 r--;
                 i--;
             }
         }
-        if(c % 3 != 0){
-            while(c % 3 != 0){
+        if(c%3!=0){
+            while(c%3!=0){
                 c--;
                 j--;
             }
         }
-        for(int k = i; k < i + 3; k++){
-            for(int l = j; l < j + 3; l++){
+        for(int k=i;k<i+3;k++){
+            for(int l=j;l<j+3;l++){
                 if(board[k][l] == temp && k != temp1 && l != temp2){
-                    System.out.println(k + "," + l + "reccheck");
+                    System.out.println(k+","+l+"reccheck");
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+    public boolean rowcheck(char[][] board,int r,int c){
+        char temp = board[r][c];
+        for(int i=0;i<9;i++){
+            if(board[r][i] == temp && i != c){
+                System.out.println(r+","+i+"rowcheck");
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean colcheck(char[][] board,int r,int c){
+        char temp = board[r][c];
+        for(int i=0;i<9;i++){
+            if(board[i][c] == temp && i != r){
+                System.out.println(i+","+c);
+                return false;
             }
         }
         return true;
