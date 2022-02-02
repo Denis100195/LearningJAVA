8b8d94ed0b1da6295d1d6f96ae73c52d86dc26f8
Denis100195 <denfil1995@gmail.com>
2022-02-02 12:57:22 +0300

    Find first unique character in a string (using HashMap)

--- src/main/java/InterviewQuestions/FirstUniqueCharacter.java
+++ src/main/java/InterviewQuestions/FirstUniqueCharacter.java
@@ -1,5 +1,7 @@
 package InterviewQuestions;
 
+import java.util.HashMap;
+
 public class FirstUniqueCharacter {
     public int firstUniqChar(String s) {
         for(int i = 0; i < s.length(); i++) {
@@ -7,6 +9,23 @@ public class FirstUniqueCharacter {
             if(s.indexOf(c) == s.lastIndexOf(c)) {
                 return i;
             }
+        }
+        return -1;
+    }
+// Implementation using HashMap
+    public int firstUniqChar2(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        // build hash map : character and how often it appears
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            if (count.get(s.charAt(i)) == 1)
+                return i;
         }
         return -1;
     }
